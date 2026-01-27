3168bc340573101f8324ea85d899ef47808907a6
Rio Andika Andriansyah <andikario0101@gmail.com>
2026-01-27 14:44:57 +0700

    feat: Implement fish data model and RecyclerView adapter for displaying fish list

---
+++ app/src/main/java/com/pab/nuraki_android/Fish.kt
@@ -0,0 +1,10 @@
+package com.pab.nuraki_android
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Fish(
+    val name: String,
+    val description: String,
+    val photo: Int
+) : Parcelable
--- app/src/main/java/com/pab/nuraki_android/MainActivity.kt
+++ app/src/main/java/com/pab/nuraki_android/MainActivity.kt
@@ -6,18 +6,47 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvFish: RecyclerView
+    private val list = ArrayList<Fish>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        rvFish = findViewById(R.id.rv_fish)
+        rvFish.setHasFixedSize(true)
+        list.addAll(getListFish())
+        showRecyclerList()
+    }
+
+    private fun getListFish(): ArrayList<Fish> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+        val listFish = ArrayList<Fish>()
+        for (i in dataName.indices) {
+            val fish = Fish(dataName[i], dataDescription[i], dataPhoto.getResourceId(i, -1))
+            listFish.add(fish)
+        }
+        return listFish
+    }
+
+    private fun showRecyclerList() {
+        rvFish.layoutManager = LinearLayoutManager(this)
+        val listFishAdapter = ListFishAdapter(list)
+        rvFish.adapter = listFishAdapter
     }
 }
